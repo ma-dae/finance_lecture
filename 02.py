502712328cf4d69f5b04ed4d4be092602ded3545
maeumdaero <kim45367325@gmail.com>
2025-09-11 22:14:12 +0900

    Add 02.py

---
+++ 02.py
@@ -0,0 +1,24 @@
+#install 경우 아나콘다 터미널에서 진행
+# import numpy as np
+# data = [1, 2, 3]
+# arr = np.array(data)
+# print(arr)
+# print(type(arr))
+
+# #1. for문 사용
+# data = [1, 2, 3]
+
+# result = []
+# for i in data:
+#     result.append(i*10)
+#     print('for result:', result)
+# print('final result:', result)
+
+import numpy as np
+arr = np.array([1, 2, 3])
+result = arr * 10
+print(result)
+# 자동 코딩 해주는 거 np 자꾸 노로 만드네
+
+git practice
+
